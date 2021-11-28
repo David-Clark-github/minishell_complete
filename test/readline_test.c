6a15e693bd747e1201702f0354cbbe496b978300
dclark <dclark@student.42.fr>
2021-11-28 17:05:23 +0100

    test of readline function

--- test/readline_test.c
+++ test/readline_test.c
@@ -6,10 +6,11 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 18:02:01 by dclark            #+#    #+#             */
-/*   Updated: 2021/11/28 14:50:03 by dclark           ###   ########.fr       */
+/*   Updated: 2021/11/28 17:03:06 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -20,5 +21,11 @@ int main(/*int ac, char **av*/)
 	char	*prompt;
 	prompt = readline("Enter input: ");
 	add_history(prompt);
-	printf("%s\n", prompt);
+	if (strlen(prompt) == 0) {
+		printf("Empty line\n");
+	}
+	else {
+		for (int i = 0; prompt[i]; i++)
+			printf("[%c]\n", prompt[i]);
+	}
 }
