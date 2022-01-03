dc32444312d947109f8e88109b056f7d8d9a5725
dclark <dclark@student.42.fr>
2022-01-03 17:25:58 +0100

    adding feature to tab_to_list and print_list accordingly.

--- srcs/print_lst.c
+++ srcs/print_lst.c
@@ -6,7 +6,7 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 13:23:22 by dclark            #+#    #+#             */
-/*   Updated: 2022/01/03 16:58:21 by dclark           ###   ########.fr       */
+/*   Updated: 2022/01/03 17:15:11 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ void	print_lst(t_lst **head)
 	{
 		if (p->log < 8 && p->log > 0)
 			printf("CMD = %s\n", p->str);
+		else if (p->log > 7 && p->log < 13)
+			printf("Redirection = %s\n", p->str);
 		else
-			printf("string = %s\n", p->str);
+			printf("String = %s\n", p->str);
 		printf("log = %d\n\n", p->log);
 		p = p->next;
 	}
--- srcs/tab_to_list.c
+++ srcs/tab_to_list.c
@@ -6,7 +6,7 @@
 /*   By: david <dclark@student.42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/24 12:31:48 by david             #+#    #+#             */
-/*   Updated: 2022/01/03 16:55:10 by dclark           ###   ########.fr       */
+/*   Updated: 2022/01/03 17:25:42 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,25 +26,35 @@ static int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	return (0);
 }
 
-int	diff_cmd(char *str)
+static int	diff_cmd_redir(char *str)
 {
 	int	flag;
 
 	flag = 0;
 	if (ft_strncmp(str, "echo", ft_strlen("echo")) == 0)
 		flag = 1;
-	if (ft_strncmp(str, "cd", ft_strlen("cd")) == 0)
+	else if (ft_strncmp(str, "cd", ft_strlen("cd")) == 0)
 		flag = 2;
-	if (ft_strncmp(str, "pwd", ft_strlen("pwd")) == 0)
+	else if (ft_strncmp(str, "pwd", ft_strlen("pwd")) == 0)
 		flag = 3;
-	if (ft_strncmp(str, "export", ft_strlen("export")) == 0)
+	else if (ft_strncmp(str, "export", ft_strlen("export")) == 0)
 		flag = 4;
-	if (ft_strncmp(str, "unset", ft_strlen("unset")) == 0)
+	else if (ft_strncmp(str, "unset", ft_strlen("unset")) == 0)
 		flag = 5;
-	if (ft_strncmp(str, "env", ft_strlen("env")) == 0)
+	else if (ft_strncmp(str, "env", ft_strlen("env")) == 0)
 		flag = 6;
-	if (ft_strncmp(str, "exit", ft_strlen("exit")) == 0)
+	else if (ft_strncmp(str, "exit", ft_strlen("exit")) == 0)
 		flag = 7;
+	else if (ft_strncmp(str, ">", ft_strlen(">")) == 0)
+		flag = 8;
+	else if (ft_strncmp(str, "<", ft_strlen("<")) == 0)
+		flag = 9;
+	else if (ft_strncmp(str, ">>", ft_strlen(">>")) == 0)
+		flag = 10;
+	else if (ft_strncmp(str, "<<", ft_strlen("<<")) == 0)
+		flag = 11;
+	else if (ft_strncmp(str, "|", ft_strlen("|")) == 0)
+		flag = 12;
 	return (flag);
 }
 
@@ -55,15 +65,15 @@ t_lst	*tab_to_list(char **tabx)
 	
 	list = NULL;
 	i_tab = 0;
-	if (tabx[i_tab] && diff_cmd(tabx[i_tab]) == 0)
+	if (!tabx[i_tab])
 		return (NULL);
 	else
 	{
-		add_lst_back(&list, tabx[i_tab], diff_cmd(tabx[i_tab]));
-		i_tab++;
+		//add_lst_back(&list, tabx[i_tab], diff_cmd(tabx[i_tab]));
+		//i_tab++;
 		while (tabx[i_tab])
 		{
-			add_lst_back(&list, tabx[i_tab], 0);
+			add_lst_back(&list, tabx[i_tab], diff_cmd_redir(tabx[i_tab]));
 			i_tab++;
 		}
 	}
