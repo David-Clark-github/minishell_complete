74c6a91d403b7fd485d57f8fb5cf381e1cb9a6f8
dclark <dclark@student.42.fr>
2022-01-03 14:07:04 +0100

    rajout de fichier non autorisé ?

---
+++ srcs/add_lst_bck2.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   add_lst_bck2.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/15 11:59:54 by dclark            #+#    #+#             */
+/*   Updated: 2022/01/03 14:02:09 by dclark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.h"
+void	add_lst_back(t_lst **head_lst, char *str, int log)
+{
+	t_lst	*dest;
+	t_lst	*pointeur;
+
+	if (!str)
+		return;
+	pointeur = *head_lst;
+	dest = malloc(sizeof(t_lst));
+	if (dest != NULL)
+	{
+		dest->str = str;
+		dest->log = log;
+		dest->next = NULL;
+	}
+	if (pointeur == NULL)
+	{
+		*head_lst = dest;
+		return;
+		
+	}
+	while (pointeur->next)
+		pointeur = pointeur->next;
+	pointeur->next = dest;
+}
