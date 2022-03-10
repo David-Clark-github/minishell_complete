4be1b3bf95fb06b36f000ba577708f45a67d65d4
dclark <dclark@student.42.fr>
2022-03-10 17:59:46 +0100

    signal mise à jour

--- signal/take_signal.c
+++ signal/take_signal.c
@@ -6,11 +6,12 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 15:55:39 by dclark            #+#    #+#             */
-/*   Updated: 2022/03/10 15:49:44 by dclark           ###   ########.fr       */
+/*   Updated: 2022/03/10 17:59:31 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <signal.h>
 
 static void	toto(int sig)
 {
@@ -20,21 +21,24 @@ static void	toto(int sig)
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
-		get_mini()->err_num = 130;
+		get_mini()->er_num = 130;
+		//printf("er_num: %d\n", get_mini()->er_num);
 	}
 }
 
+/*
 static void	tutu(int sig)
 {
 	if (sig == SIGQUIT)
 	{
-		printf("HELLO !\n");
-		exit(get_mini()->err_num);
+		//exit(get_mini()->er_num);
 	}
 }
+*/
 
 void	take_signal(void)
 {
-	signal(SIGQUIT, tutu);
+	//signal(SIGQUIT, tutu);
 	signal(SIGINT, toto);
+	signal(SIGQUIT, SIG_IGN);
 }
