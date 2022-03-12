2387babc724fd980c557f7d221650f47faeadac1
Dave <david.clark@dbmail.com>
2022-03-12 23:02:35 +0100

    ft_cd arg err if more than 2 argument

--- builtins/ft_cd.c
+++ builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: david <dclark@student.42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 12:26:49 by david             #+#    #+#             */
-/*   Updated: 2022/03/10 16:09:37 by dclark           ###   ########.fr       */
+/*   Updated: 2022/03/12 22:51:35 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 int	ft_cd(char *path)
 {
+	if (ft_tablen(get_mini->tab_separ) > 2)
+	{
+		printf("Too much argument for cd\n");
+		get_mini()->err_num = 1;
+		return (EXIT_FAILURE);
+	}
 	if (chdir(path) == 0)
 		return (EXIT_SUCCESS);
 	else
