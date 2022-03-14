b6f9653aff65e0e982cb90b1f3691e87906d8426
dclark <dclark@student.42.fr>
2022-03-14 19:20:39 +0100

    parsing.c file deleted, parsing2.c a la norme

--- parsing/parsing2.c
+++ parsing/parsing2.c
@@ -6,7 +6,7 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 10:38:17 by dclark            #+#    #+#             */
-/*   Updated: 2022/03/14 18:28:54 by dclark           ###   ########.fr       */
+/*   Updated: 2022/03/14 19:19:56 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,7 @@ void	parsing(t_mini *mini)
 {
 	mini->exp = expension(mini);
 	if (mini->exp == NULL)
-		return;
+		return ;
 	mini->tab_separ = ft_separ(mini->exp, &mini->tab_len);
-	/*
-	if (check_tab(mini->tab_separ) == 0)
-		return;
-	*/
 	mini->list = tab_to_list(mini->tab_separ, mini->tab_len);
 }
