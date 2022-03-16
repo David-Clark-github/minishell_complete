8a0078f50a05fc8e97460be82b4b5ceda087affa
dclark <dclark@student.42.fr>
2022-03-16 17:05:46 +0100

    push le parsing

--- parsing/parsing2.c
+++ parsing/parsing2.c
@@ -6,7 +6,7 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 10:38:17 by dclark            #+#    #+#             */
-/*   Updated: 2022/03/16 14:56:45 by dclark           ###   ########.fr       */
+/*   Updated: 2022/03/16 15:26:43 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,14 @@ void	parsing(t_mini *mini)
 		return ;
 		*/
 	mini->tab_separ = ft_separ(mini->prompt, &mini->tab_len);
-	while (mini->tab_separ[i])
+	while (mini->tab_separ[i] && mini->int_separ[i] != 1)
 	{
 		if (mini->int_separ[i] != 1)
-			mini->tab_separ[i] = expension(mini->tab_separ[i]);
-		i++;
+		{
+				if (i != 0 && (mini->int_separ[i] != 2 || diff_redir(mini->tab_separ[i - 1]) != 10))
+					mini->tab_separ[i] = expension(mini->tab_separ[i]);
+		}
+			i++;
 	}
 	mini->list = tab_to_list(mini->tab_separ, mini->tab_len);
 }
