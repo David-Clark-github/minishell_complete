188afb4a1027f6140f35643e7d49327849937468
dclark <dclark@student.42.fr>
2022-03-18 17:46:19 +0100

    merge ft_exit.c

--- builtins/ft_exit.c
+++ builtins/ft_exit.c
@@ -3,20 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
+/*   By: seciurte <seciurte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 11:27:43 by dclark            #+#    #+#             */
-/*   Updated: 2022/03/18 12:21:36 by dclark           ###   ########.fr       */
+/*   Updated: 2022/03/18 17:44:18 by seciurte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_exit(int num, int print, int fd)
+int	ft_exit(int num)
 {
-	get_mini()->er_num = num;
 	ft_free_mini(get_mini(), 0);
-	if (print == 1)
-		write(fd, "exit", ft_strlen("exit"));
-	exit(get_mini()->er_num);
+	write(STDERR_FILENO, "exit\n", 5);
+	exit(num);
 }
