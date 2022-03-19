8b03efa0be5e19a245ae19b581f81da17e45740a
dclark <dclark@student.42.fr>
2022-03-19 14:56:16 +0100

    test_pipe

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: seciurte <seciurte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 13:28:08 by dclark            #+#    #+#             */
-/*   Updated: 2022/03/19 12:50:27 by seciurte         ###   ########.fr       */
+/*   Updated: 2022/03/19 14:22:55 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,47 +42,47 @@ int	main(int ac, char **av, char **ev)
 	{
 		mini->prompt = readline("\33[2K\rMinishell~ ");
 		if (mini->prompt == NULL)
-			ft_exit(mini->er_num);
+			ft_exit(g_err_num);
 		if (ft_strlen(mini->prompt) != 0)
 		{
 			add_history(mini->prompt);
-			mini->er_num = check_error_quotes(mini->prompt, &mini->er_num);
+			g_err_num = check_error_quotes(mini->prompt, &g_err_num);
 		}
-		if (mini->er_num != 0)
-			printf("er_num = %d error de quotes\n", mini->er_num);
-		if (ft_strlen(mini->prompt) != 0 && mini->er_num == 0)
+		if (g_err_num != 0)
+			printf("er_num = %d error de quotes\n", g_err_num);
+		if (ft_strlen(mini->prompt) != 0 && g_err_num == 0)
 		{
-			// if (mini->er_num != 0)
-			// 	printf("er_num = %d error de quotes\n", mini->er_num);
-			if (mini->er_num != -1)
+			// if (g_err_num != 0)
+			// 	printf("er_num = %d error de quotes\n", g_err_num);
+			if (g_err_num != -1)
 				parsing(mini);
-			// if (mini->list == NULL && mini->er_num == 0)
+			// if (mini->list == NULL && g_err_num == 0)
 			// 	printf("error during parsing\n");
 			// if (check_syntax(mini) == EXIT_FAILURE)
 			// 	printf("syntax error\n");
-			// if (mini->er_num != -1 && extract_lst(mini->list))
+			// if (g_err_num != -1 && extract_lst(mini->list))
 			// if (mini->list && mini->list->log == 4)
 			// 	ft_export(name, &mini->cp_ev);
 			// if (mini->list && mini->list->log == 5)
 			// 	ft_unset(name);
-			// if (mini->list && mini->er_num != -1 && mini->list->log == 6)
+			// if (mini->list && g_err_num != -1 && mini->list->log == 6)
 			// 	ft_env(mini->cp_ev, 1);
-			// if (mini->er_num == 0 && mini->list->log == 3)
+			// if (g_err_num == 0 && mini->list->log == 3)
 			//  	ft_pwd(1);
-			// if (mini->er_num == 0 && mini->list->log == 2)
+			// if (g_err_num == 0 && mini->list->log == 2)
 			// 	ft_cd(mini->tab_separ[1]);
-			//  if (mini->er_num == 0 && mini->list->log == 6)
+			//  if (g_err_num == 0 && mini->list->log == 6)
 			//  	ft_env(mini->cp_ev, 1);
-			// if (mini->er_num == 0 && mini->list->log == 4)
+			// if (g_err_num == 0 && mini->list->log == 4)
 			// 	arg_export(mini->tab_separ, mini);
-			// if (mini->er_num != 0)
-			// 	printf("er_num = %d\n", mini->er_num);
+			// if (g_err_num != 0)
+			// 	printf("er_num = %d\n", g_err_num);
 			// ft_freetab(mini->tab_separ);
 			// print_lst(&mini->list);
 			exec_instructions(mini);
 		}
-		if (check_error_quotes(mini->prompt, &mini->er_num) != 0)
-			mini->er_num = 0;
+		if (check_error_quotes(mini->prompt, &g_err_num) != 0)
+			g_err_num = 0;
 		ft_free_mini(mini, 1);
 	}
 }
---
+++ test_pipe.sh
@@ -0,0 +1,6 @@
+./minishell << end
+echo > /dev/full | export tutu=tutu
+echo \$?
+end
+#cat minish
+# diff minish vraish
