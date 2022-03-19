1a1389b4a1db76549c8da808595cce81d3818835
dclark <dclark@student.42.fr>
2022-03-19 10:48:06 +0100

    ft_env.C avec message d'erreur

--- builtins/ft_env.c
+++ builtins/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: david <dclark@student.42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 17:39:38 by david             #+#    #+#             */
-/*   Updated: 2022/01/25 12:34:38 by dclark           ###   ########.fr       */
+/*   Updated: 2022/03/19 10:41:55 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,23 @@
 int	ft_env(char **tab_env, int fd)
 {
 	int	i;
+	int	res_w;
 
 	i = 0;
 	while (tab_env[i])
 	{
-		write(fd, tab_env[i], strlen(tab_env[i]));
-		write(fd, "\n", 1);
+		res_w = write(fd, tab_env[i], strlen(tab_env[i]));
+		if (res_w == -1)
+		{
+			write(2, "Minishell: write error: No space left on device\n", 48);
+			return (125);
+		}
+		res_w = write(fd, "\n", 1);
+		if (res_w == -1)
+		{
+			write(2, "Minishell: write error: No space left on device\n", 48);
+			return (125);
+		}
 		i++;
 	}
 	return (EXIT_SUCCESS);
